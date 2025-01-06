0be23d08407395288eaf1df207af422018bc202c
Muhammad-Anique <anique.cs@gmail.com>
2025-01-07 00:50:41 +0500

    Deployment 1

--- src/app/lib/supabase.js
+++ src/app/lib/supabase.js
@@ -1,8 +1,6 @@
 // lib/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://exggfeypqucbibabxodx.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4Z2dmZXlwcXVjYmliYWJ4b2R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5Mzk0OTIsImV4cCI6MjA0OTUxNTQ5Mn0.nK-mDPglDIuQenvgSVm1vCtu8op5TDUNGletBOwInZw";
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
